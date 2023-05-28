8d0f1734eb9b897c6b1bdd1224c48aa7dcbb426d
JJ <jonathanjamesholt@gmail.com>
2023-05-28 22:36:01 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod clipboard;
+mod svg;
+mod config;
+mod style;
+mod keybind;
+
+fn main() {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn check_inkscape_is_active() {
+        unimplemented!();
+    }
+    #[test]
+    fn take_input() {
+        unimplemented!();
+    }
+    #[test]
+    fn produce_svg() {
+        unimplemented!();
+    }
+    #[test]
+    fn add_to_clipboard() {
+        unimplemented!();
+    }
+    #[test]
+    fn automatically_paste_clipboard() {
+        unimplemented!();
+    }
+}
