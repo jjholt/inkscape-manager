d694e85b952c67d8f2a410f2fb45d2b79377470c
JJ <jonathanjamesholt@gmail.com>
2023-05-28 22:56:10 +0100

    added wrapper over array of styles for nicer syntax

--- src/style.rs
+++ src/style.rs
@@ -8,18 +8,20 @@ pub struct Style<'a> {
     pub value: &'a str,
 }
 
+pub struct StyleList<'a,'b> {
+    styles: &'a [Style<'b>]
+}
+
 impl <'a> fmt::Display for Style<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}:{};", self.style, self.value)
     }
 }
 
-pub trait DisplayVec {
-    fn display(&self) -> String; 
-}
-
-impl <'a> DisplayVec for Vec<Style<'a>> {
-    fn display(&self) -> String {
-        self.iter().map(|style| style.to_string()).collect()
+impl <'a,'b> fmt::Display for StyleList<'a,'b> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let concat_strings: String = self.styles.iter().map(|style| style.to_string()).collect();
+        write!(f, "{}", concat_strings)
     }
 }
+
--- src/svg.rs
+++ src/svg.rs
@@ -1,14 +1,14 @@
-use crate::style::{Style, DisplayVec} ;
+use crate::style::{Style, StyleList} ;
 
-pub struct SVG <'a> {
-    styles: Vec<Style<'a>>,
+pub struct SVG <'a, 'b> {
+    styles: StyleList<'a,'b>
 }
 
-impl <'a> SVG<'a> {
+impl <'a, 'b> SVG<'a, 'b> {
     pub fn output(&self) -> String {
         format!("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><svg><defs/><g></g>\
                 <inkscape:clipboard style=\"{}\" /> </svg>",
-                self.styles.display()
+                self.styles
                )
     }
 }
@@ -18,25 +18,25 @@ mod tests {
     use super::*;
     #[test]
     fn display_style() {
-        let my_style = Style { key: "fill", value: "#000" };
+        let my_style = Style { style: "fill", value: "#000" };
         assert_eq!(my_style.to_string(), "fill:#000;");
     }
     #[test]
     fn iterates() {
-        let my_styles = vec![
-            Style { key: "fill", value: "#000" },
-            Style { key: "stroke", value: "#000" },
-            Style { key: "stroke-width", value: "0.7" },
-        ];
+        let my_styles: StyleList = StyleList { styles: &vec![
+            Style { style: "fill", value: "#000" },
+            Style { style: "stroke", value: "#000" },
+            Style { style: "stroke-width", value: "0.7" },
+        ]};
         assert_eq!(
-            format!("{}", my_styles.display()),
+            format!("{}", my_styles),
            "fill:#000;stroke:#000;stroke-width:0.7;"
        );
     }
     #[test]
     fn correct_output() {
-        let styles = vec![Style { key: "fill", value: "#000" }];
-        let my_svg = SVG { styles };
+        let styles = &vec![Style { style: "fill", value: "#000" }];
+        let my_svg = SVG { styles: StyleList{styles} };
 
         assert_eq!(
             "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><svg><defs/><g></g>\
