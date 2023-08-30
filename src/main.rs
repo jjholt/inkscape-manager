f4655f6882c5f9378fd46085cd7643da01ffb71b
JJ <jonathanjamesholt@gmail.com>
2023-08-30 15:17:16 +0100

    Added support for all keys to be bound

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,12 @@ use event_handler::EventHandler;
 
 use std::error::Error;
 
-// fn read(filename: &Path) -> Result<String, Box<dyn Error>> {
-//     let mut file = File::open(filename)?;
-//     let mut contents = String::new();
-//     file.read_to_string(&mut contents)?;
-//     Ok(contents)
-// }
-
 fn main() -> Result<(), Box<dyn Error>> {
     let target = Some("image/x-inkscape-svg");
     let config_str = Config::get_raw_string()?;
     let config: Config = serde_yaml::from_str(&config_str)?;
 
     println!("Keybinds:\n{}", &config.keybinds);
-    // if let Some(target) = &config.target {
-    //     println!("Target:\n{}", target);
-    // }
 
     let (xcb, screen_num) = xcb::Connection::connect(None)?;
     let connections = Connections::setup(&xcb, screen_num)?;
